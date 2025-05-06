8767d6eca1978f60498e0866193a4dfe5cc4fdcb
Anton Dvorson <dvorson@gmail.com>
2025-05-06 19:05:52 +0200

    Publish post Test Post

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import Index from './Pages/Index.jsx'
-import * as serviceWorker from './serviceWorker'
-
-ReactDOM.render(<Index />, document.getElementById('app'))
-
-serviceWorker.unregister()
